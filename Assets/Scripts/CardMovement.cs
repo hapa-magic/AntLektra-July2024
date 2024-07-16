b906fb4e2d3b5ece6c1b6959775ea73a086b81be
hapa-magic <97630257+hapa-magic@users.noreply.github.com>
2024-07-15 22:27:33 -0700

    Adjusted card movement, laid groundwork to play cards

--- Assets/Scripts/CardMovement.cs
+++ Assets/Scripts/CardMovement.cs
@@ -1,55 +1,223 @@
-using Unity.VisualScripting;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private RectTransform rectTransform;
     private Canvas canvas;
-    private Vector2 originalLocalPointerPosition;
-    private Vector3 originalPanelLocalPosition;
+    private RectTransform canvasRectTranform;
     private Vector3 originalScale;
     private int currentState = 0;
     private Quaternion originalRotation;
     private Vector3 originalPosition;
+    int UILayer;
+    private GameObject playTarget;
+    
+    [SerializeField] private Vector3 movementOffset = new Vector3 (-300, 280, 0);
     [SerializeField] private float selectScale = 1.1f;
     [SerializeField] private Vector2 cardPlay;
-    [SerializeField] private Vector2 playPosition;
+    [SerializeField] private Vector3 playPosition;
     [SerializeField] private GameObject glowEffect;
     [SerializeField] private GameObject playArrow;
+    [SerializeField] private float lerpFactor = 0.1f;
+    [SerializeField] private int cardPlayDivider = 4;
+    [SerializeField] private float cardPlayMultiplier = 1f;
+    [SerializeField] private bool needUpdateCardPlayPosition = false;
+    [SerializeField] private int playPositionYDivider = 2;
+    [SerializeField] private float playPositionYMultiplier = 1f;
+    [SerializeField] private int playPositionXDivider = 4;
+    [SerializeField] private float playPositionXMultiplier = 1f;
+    [SerializeField] private bool needUpdatePlayPosition = false;
 
     void Awake()
     {
+        UILayer = LayerMask.NameToLayer("UI");
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
-        originalScale = transform.localScale;
-        originalPosition = transform.localPosition;
-        originalRotation = transform.localRotation;
+
+        if (canvas != null)
+        {
+            canvasRectTranform = canvas.GetComponent<RectTransform>();
+        }
+
+        originalScale = rectTransform.localScale;
+        originalPosition = rectTransform.localPosition;
+        originalRotation = rectTransform.localRotation;
+
+        updateCardPlayPostion();
+        updatePlayPostion();
     }
 
     void Update()
     {
+        if (needUpdateCardPlayPosition)
+        {
+            updateCardPlayPostion();
+        }
+
+        if (needUpdatePlayPosition)
+        {
+            updatePlayPostion();
+        }
+        
         switch (currentState)
         {
             case 1:
                 HandleHoverState();
                 break;
-
             case 2:
                 HandleDragState();
-                if (!Input.GetMouseButton(0))
+                if (IsPointerOverUIElement()) {
+                    if (!Input.GetMouseButton(0)) {
+                        CheckForPlay();
+                    }
+                }
+                if (!Input.GetMouseButton(0)) //Check if mouse button is released
                 {
-                    TransisitonToState0();
+                    TransitionToState0();
                 }
                 break;
-
-            case 3:
+            /*case 3:
                 HandlePlayState();
-               break;
+                if (!Input.GetMouseButton(0)) //Check if mouse button is released
+                {
+                    TransitionToState0();
+                }
+                break;*/
+            case 4:
+                HandleAlmostPlayState();  
+                break;  
         }
     }
 
-    private void HandleHoverState()
+    private void TransitionToState0()
+    {
+        currentState = 0;
+        rectTransform.localScale = originalScale; //Reset Scale
+        rectTransform.localRotation = originalRotation; //Reset Rotation
+        rectTransform.localPosition = originalPosition; //Reset Position
+        glowEffect.SetActive(false); //Disable glow effect
+        playArrow.SetActive(false); //Disable playArrow
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (currentState == 0)
+        {
+            originalPosition = rectTransform.localPosition;
+            originalRotation = rectTransform.localRotation;
+            originalScale = rectTransform.localScale;
+
+            currentState = 1;
+        } 
+    }
+    public void OnPointerUp(PointerEventData eventData) {
+        if (currentState == 2) {
+            Debug.Log("Anything");
+            CheckForPlay();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (currentState == 1)
+        {
+            TransitionToState0();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (currentState == 1)
+        {
+            currentState = 2;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (currentState == 2)
+        {
+            /*if (Input.mousePosition.y > cardPlay.y)
+            {
+                currentState = 3;
+                playArrow.SetActive(true);
+                rectTransform.localPosition = Vector3.Lerp(rectTransform.position, playPosition, lerpFactor);
+            }*/
+        }
+    }
+    public bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+    public bool CheckForPlay() {
+        return CheckForPlay(GetEventSystemRaycastResults());
+    }
+    private bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == UILayer)
+            {
+                if (curRaysastResult.gameObject.GetComponent<PlaySpot>() != null)
+                {
+                    playTarget = curRaysastResult.gameObject;
+                    rectTransform.localPosition = playTarget.GetComponent<RectTransform>().localPosition + movementOffset;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private bool CheckForPlay(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == UILayer)
+            {
+                if (curRaysastResult.gameObject.GetComponent<PlaySpot>() != null)
+                {
+                    playTarget = curRaysastResult.gameObject;
+                    rectTransform.localPosition = playTarget.GetComponent<RectTransform>().localPosition + movementOffset;
+                    //ChangeParent();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
+
+    private void HandleAlmostPlayState() {
+        /*List<RaycastResult> eventSystemRaysastResults = GetEventSystemRaycastResults();
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == UILayer)
+            {
+                if (curRaysastResult.gameObject.GetComponent<PlaySpot>() != null)
+                {
+                    Debug.Log(curRaysastResult.gameObject);
+                    Debug.Log("HEEEELPO");
+                    //rectTransform.localPosition = curRaysastResult.gameObject.RectTransform;
+                }
+            }
+        }*/
+    }
+
+private void HandleHoverState()
     {
         glowEffect.SetActive(true);
         rectTransform.localScale = originalScale * selectScale;
@@ -57,9 +225,11 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
 
     private void HandleDragState()
     {
+        //Set the card's rotation to zero
         rectTransform.localRotation = Quaternion.identity;
+        rectTransform.position = Vector3.Lerp(rectTransform.position, Input.mousePosition, lerpFactor);
     }
-    
+
     private void HandlePlayState()
     {
         rectTransform.localPosition = playPosition;
@@ -72,61 +242,25 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
         }
     }
 
-    private void TransisitonToState0()
+    private void updateCardPlayPostion()
     {
-        currentState = 0;
-        rectTransform.localScale = originalScale;
-        rectTransform.localPosition = originalPosition;
-        rectTransform.localRotation = originalRotation;
-        glowEffect.SetActive(false);
-        playArrow.SetActive(false);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        if (currentState == 1)
+        if (cardPlayDivider != 0 && canvasRectTranform != null)
         {
-            TransisitonToState0();
-        }
-    }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        if (currentState == 0)
-        {
-            originalPosition = rectTransform.localPosition; 
-            originalRotation = rectTransform.localRotation;
-            originalScale = rectTransform.localScale;
+            float segment = cardPlayMultiplier / cardPlayDivider;
 
-            currentState = 1;
+            cardPlay.y = canvasRectTranform.rect.height * segment;
         }
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    private void updatePlayPostion()
     {
-        if (currentState == 1)
+        if (canvasRectTranform != null && playPositionYDivider != 0 && playPositionXDivider != 0)
         {
-            currentState = 2;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), 
-                eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition);
-            originalPanelLocalPosition = rectTransform.localPosition;
+            float segmentX = playPositionXMultiplier / playPositionXDivider;
+            float segmentY = playPositionYMultiplier / playPositionYDivider;
+
+            playPosition.x = canvasRectTranform.rect.width * segmentX;
+            playPosition.y = canvasRectTranform.rect.height * segmentY;
         }
     }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (currentState == 2)
-        {
-            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(),
-                eventData.position, eventData.pressEventCamera, out Vector2 localPointerPosition))
-            {
-                localPointerPosition /= canvas.scaleFactor;
-
-                Vector3 offsetOriginal = localPointerPosition - originalLocalPointerPosition;
-                rectTransform.localPosition = originalPanelLocalPosition + offsetOriginal;
-
-            }
-        }
-    }
-
-
 }
--- Assets/Scripts/DeckManager.cs
+++ Assets/Scripts/DeckManager.cs
@@ -1,6 +1,6 @@
-using HapaMagic;
 using System.Collections;
 using System.Collections.Generic;
+using HapaMagic;
 using TMPro;
 using UnityEngine;
 
@@ -8,28 +8,44 @@ public class DeckManager : MonoBehaviour
 {
     public List<Card> allCards = new List<Card>();
 
+    public int startingHandSize = 5;
+
     private int currentIndex = 0;
-    public HandManager handManager;
+    public int maxHandSize = 5;
     public EggTracker eggTracker;
     public int eggCost;
+    public int currentHandSize;
+    private HandManager handManager;
     public TMP_Text drawCostText;
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        // Load all card assets
-        Card[] cards = Resources.LoadAll<Card>("Card Data");
+        //Load all card assets from the Resources folder
+        Card[] cards = Resources.LoadAll<Card>("Cards");
 
+        //Add the loaded cards to the allCards list
         allCards.AddRange(cards);
-        for (int i = 0; i < 5; i++)
+
+        handManager = FindObjectOfType<HandManager>();
+        maxHandSize = handManager.maxHandSize;
+        for (int i = 0; i < startingHandSize; i++)
         {
+            Debug.Log($"Drawing Card");
             DrawCard(handManager);
         }
-
         eggCost = 10;
         UpdateEggCostNum();
         StartCoroutine(DecayEggCost());
     }
 
+    void Update()
+    {
+        if (handManager != null)
+        {
+            currentHandSize = handManager.cardsInHand.Count;
+        }
+    }
     public void PayToDraw()
     {
         if (eggTracker.eggNum > eggCost)
@@ -40,17 +56,19 @@ public class DeckManager : MonoBehaviour
             UpdateEggCostNum();
         }        
     }
-
     public void DrawCard(HandManager handManager)
     {
-        if (allCards.Count == 0) return;
+        if (allCards.Count == 0)
+            return;
 
-        Card nextCard = allCards[currentIndex];
-        handManager.AddCardToHand(nextCard);
-        currentIndex = (currentIndex+ 1) % allCards.Count;
-
+        if (currentHandSize < maxHandSize)
+        {
+            Card nextCard = allCards[currentIndex];
+            handManager.AddCardToHand(nextCard);
+            currentIndex = (currentIndex + 1) % allCards.Count;
+        }
     }
-    public void UpdateEggCostNum()
+        public void UpdateEggCostNum()
     {
         drawCostText.text = "$ " + eggCost.ToString();
     }
@@ -67,4 +85,3 @@ public class DeckManager : MonoBehaviour
         }
     }
 }
-
