06f862462b01b9dfbbf21f6b19d34edfb9f7cb1d
hapa-magic <97630257+hapa-magic@users.noreply.github.com>
2024-07-19 15:23:52 -0700

    Reorganized

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private int playerHealth = 20;
+    private int playerEggs;
+    private int difficulty = 5;
+
+    public OptionsManager OptionsManager { get; private set; }
+    public AudioManager AudioManager { get; private set; }
+    public DeckManager DeckManager { get; private set; }
+
+    public bool PlayingCard = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            Debug.Log($"Setting Instance to {this}");
+            InitializeManagers();
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void InitializeManagers()
+    {
+        OptionsManager = GetComponentInChildren<OptionsManager>();
+        AudioManager = GetComponentInChildren<AudioManager>();
+        DeckManager = GetComponentInChildren<DeckManager>();
+
+        if (OptionsManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/OptionsManager");
+            if (prefab == null)
+            {
+                Debug.Log($"OptionsManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                OptionsManager = GetComponentInChildren<OptionsManager>();
+            }
+        }
+        if (AudioManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/AudioManager");
+            if (prefab == null)
+            {
+                Debug.Log($"AudioManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                AudioManager = GetComponentInChildren<AudioManager>();
+            }
+        }
+        if (DeckManager == null)
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/DeckManager");
+            if (prefab == null)
+            {
+                Debug.Log($"DeckManager prefab not found");
+            }
+            else
+            {
+                Instantiate(prefab, transform.position, Quaternion.identity, transform);
+                DeckManager = GetComponentInChildren<DeckManager>();
+            }
+        }
+    }
+
+    public int PlayerHealth
+    {
+        get { return playerHealth; }
+        set { playerHealth = value; }
+    }
+
+    public int PlayerEggs
+    {
+        get { return playerEggs; }
+        set { playerEggs = value; }
+    }
+
+    public int Difficulty
+    {
+        get { return difficulty; }
+        set { difficulty = value; }
+    }
+}
